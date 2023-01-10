8598cfccc6e86511c0804c91ce265d3c62ea7ed9
Vishal Kumar <vy10607@gmail.com>
2023-01-10 14:53:27 +0530

    Auth Controller - login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,17 @@
 import User from "../models/user.schema";
 import asyncHandler from "../services/asyncHandler";
 import CustomError from "../utils/customError";
-import cokkieOptions from "../utils/cookieOptions";
+import cookieOptions from "../utils/cookieOptions";
 
 /********************************************************
- * @signup
+ * @SIGNUP
  * @route https://localhost:4000/api/auth/signup
  * @description User signup controller for creating a new user
  * @parameters name, email, password
  * @return User Object
 *********************************************************/
 
-export const signUp = asyncHandler(async (req, res) => {
+export const signup = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body;
 
     if (!name || !email || !password){
@@ -31,10 +31,48 @@ export const signUp = asyncHandler(async (req, res) => {
         password
     })
 
-    const token = user.getJwtTokken();
+    const token = user.getJwtToken();
     user.password = undefined;
 
     res.cookie("token", token, cokkieOptions);
+    res.status(200).json({
+        success: true,
+        token,
+        user
+    })
+})
+
+/********************************************************
+ * @LOGIN
+ * @route https://localhost:4000/api/auth/login
+ * @description User login controller for login a user
+ * @parameters email, password
+ * @return User Object
+*********************************************************/
+
+export const login = asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+
+    if (!email || !password){
+        throw new CustomError("Please fill all fields", 400);
+    }
+
+    const user = await User.findOne({email}).select("+password");
+    
+    if (!user) {
+        throw new CustomError("Invalid credentials", 400);
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched){
+        throw new CustomError("Invalid credentials - pass", 400);
+    }
+
+    const token = user.getJwtToken();
+    user.password = undefined;
+    res.cookie("token", token, cookieOptions);
+
     res.status(200).json({
         success: true,
         token,
--- utils/cookieOptions.js
+++ utils/cookieOptions.js
@@ -1,6 +1,6 @@
-const cokkieOptions = {
+const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     httpOnly: true,
 }
 
-export default cokkieOptions;
+export default cookieOptions;
