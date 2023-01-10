a427d77f894a3bc1017708e579caf63ff2714dfa
Vishal Kumar <vy10607@gmail.com>
2023-01-10 12:09:59 +0530

    Added order schema

---
+++ models/order.schema.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import orderStatus from "../utils/orderStatus";
+
+const orderSchema = mongoose.Schema(
+    {
+        products: {
+            type: [
+                {
+                    productId: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "Product",
+                        required: true,
+                    },
+                    count: Number,
+                    price: Number,
+                }
+            ],
+            required: true,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        Address: {
+          type: String,
+          required: true,  
+        },
+        phoneNumber: {
+            type: Number,
+            required: true,
+        },
+        amount: {
+            type: Number,
+            required: true,
+        },
+        coupon: String,
+        transactionId: String,
+        status: {
+            type: String,
+            enum: Object.values(orderStatus),
+            default: orderStatus.ORDERED,
+        },
+        // paymentMode: UPI, card, wallet, or COD
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default mongoose.model("Order", orderSchema);
